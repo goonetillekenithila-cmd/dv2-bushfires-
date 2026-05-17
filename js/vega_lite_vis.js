8e3b2232f262c70790a5068bb7e5c500b9f44fc0
goone <ngoo0007@student.monash.edu>
2026-05-17 18:51:59 +1000

    heatmap addition

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,2 +1,5 @@
 var opening_hook = "charts/01_opening_hook.vg.json";
+var seasonality_heatmap = "charts/02_seasonality_heatmap.vg.json";
+
 vegaEmbed("#opening_hook", opening_hook, { "actions": false }).catch(console.error);
+vegaEmbed("#seasonality_heatmap", seasonality_heatmap, { "actions": false }).catch(console.error);
